39a3df83d65e21833950de8e447b47ffc9d30cb3
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-16 16:07:55 +0530

    Creating class VoterImpl in package com.election

--- src/com/example/Voter.java
+++ src/com/example/Voter.java
@@ -5,8 +5,33 @@ public class Voter {
     private  int age;
     static final int VOTER_ELIGIBLE_AGE = 18;
 
-    Voter(){
+    public Voter(){
+        String name;
+        int age;
 
     }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public void getAgeCriteria(final int VOTER_ELIGIBLE_AGE){
+       if (getAge()>=VOTER_ELIGIBLE_AGE){
+           System.out.println(name+" Is Eligible to Vote");
+       } else if (getAge()>0&& getAge()<VOTER_ELIGIBLE_AGE) {
+           System.out.println(name+" Is not Eligible to vote");
+       }
+       else {
+           System.out.println("Age Can't Be negative or Zero");
+       }
+    }
 
-}
+
+    }
